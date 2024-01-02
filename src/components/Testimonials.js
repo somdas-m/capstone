17a4e5b0b92b5303c85fac9990acd8bfdf07775b
App Dev <geek.coder.93@gmail.com>
2024-01-02 23:15:40 +0530

    updated the testimonial rating images

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,33 +1,39 @@
-import rating from "../images/5star.png"
+import rating5 from "../images/5star.png"
+import rating4 from "../images/4star.png"
+import rating3 from "../images/3star.png"
 import userImg1 from "../images/userImg.avif"
 
 export const Testimonials = (props) => {
     const userReviews = [{
         name: "User 1",
         desc: "Excellent",
-        img: userImg1
+        img: userImg1,
+        rating: rating5
     },
     {
         name: "User 2",
         desc: "Nice",
-        img: userImg1
+        img: userImg1,
+        rating: rating4
     },
     {
         name: "User 3",
         desc: "Good",
-        img: userImg1
+        img: userImg1,
+        rating: rating3
     },
     {
         name: "User 4",
         desc: "Really awesome!",
-        img: userImg1
+        img: userImg1,
+        rating: rating5
     }]
     return <div className="container testimonials-container">
         <h1 className="testimonials-h">Testimonials</h1>
         <div className="testimonials-content">
             {
                 userReviews.map(review => {
-                    const { name, desc, img } = review
+                    const { name, desc, img, rating } = review
                     return <div class="review">
                         <img id="profile-img" src={img} alt="Customer Pic" style={{ borderRadius: "50%", marginTop: "1em" }} />
                         <h2>{name}</h2>
